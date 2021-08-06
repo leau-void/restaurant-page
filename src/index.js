407983ce467dfcda8e84256492dee439c19a5c5a
leau-void <lpierra@gmail.com>
2021-08-06 10:44:38 -0300

    tab system working, now style and create tab content

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
 import './style.css'
-import templateModule from './template.js'
-import './home.js'
-import './menu.js'
-import './contact.js'
+import template from './template.js'
+// import './home.js'
+// import './menu.js'
+// import './contact.js'
 
-templateModule();
+
+const restaurant = (function () {
+
+  const _getContainer = () => { return document.body.firstElementChild }
+
+  const _handler = () => {
+    if (!event.target.classList.contains("tabs")) return
+    let id = event.target.id
+    
+    _rebuild(id)
+  }
+  
+  const _rebuild = (currentTab) => {
+    const mainDiv = _getContainer().children[1]
+    mainDiv.replaceChild(template(currentTab), mainDiv.firstElementChild)
+  }
+  
+  const init = () => {    
+    document.body.replaceChild(template(), document.body.firstElementChild)
+    
+    _getContainer().addEventListener("click", _handler)
+  }
+  
+  return { init }
+})()
+
+restaurant.init()
+
 
 
 
--- src/template.js
+++ src/template.js
@@ -1,3 +1,80 @@
-export default function() {
-  console.log("hiii")
+const tabMixin = { home, menu, contact }
+
+function home () {
+  return "home1"
 }
+
+function menu () {
+  return "menu1"
+}
+
+function contact () {
+  return "contact1"
+}
+
+
+export default function template (tab) {
+
+  if (tab) {
+    const content = document.createElement("div")
+    content.setAttribute("id", "main-content")
+    console.log(tab)
+   // content.appendChild(tabMixin[tab]())
+
+
+    return content
+  }
+
+  tab = "home"
+
+  const container = document.createElement("div")
+  container.setAttribute("id", "content")
+  
+  const header = document.createElement("div")
+  header.setAttribute("id", "header")
+
+  const main = document.createElement("div")
+  main.setAttribute("id", "header-main")
+  main.textContent = "Au Basilic Sacré"
+  header.appendChild(main)
+  
+  const sub = document.createElement("div")
+  sub.setAttribute("id", "header-sub")
+  sub.textContent = "Herbes et Médecines"
+  header.appendChild(sub)
+  
+  const nav = document.createElement("div")
+  nav.setAttribute("id", "nav-bar")
+  
+  const home = document.createElement("button")
+  home.textContent = "home"
+  home.classList.add("tabs")
+  home.setAttribute("id", "home")
+  nav.appendChild(home)
+  
+  const menu = document.createElement("button")
+  menu.textContent = 'menu'
+  menu.classList.add("tabs")
+  menu.setAttribute("id", "menu")
+  nav.appendChild(menu)
+  
+  const contact = document.createElement("button")
+  contact.textContent = "contact"
+  contact.classList.add("tabs")
+  contact.setAttribute("id", "contact")
+  nav.appendChild(contact)
+  
+  header.appendChild(nav)
+  
+  container.appendChild(header)
+
+  const mainDiv = document.createElement("div")
+  mainDiv.setAttribute("id", "main")
+
+  mainDiv.appendChild(template(tab))
+
+  container.appendChild(mainDiv)
+  
+  return container
+}
+
